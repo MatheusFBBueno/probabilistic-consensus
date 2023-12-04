8105c313810ad9bcda182d035403c88ccedf803b
MatheusFBBueno <matfelipe2012@gmail.com>
2023-12-04 10:32:17 -0300

    feat: added byzantine node test case

---
+++ src/main/java/org/example/ByzantineNodesInit.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import ProbabilisticConsensus.ConsensusNode;
+
+public class ByzantineNodesInit {
+
+    public static void main(String[] args) {
+        int numberOfThreads = 8;
+
+
+        for (int i = 1; i <= numberOfThreads; i++) {
+            int nodeId = i+4;
+            Thread thread = new Thread(() -> {
+                try {
+                    initConsensus(nodeId);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+
+            thread.start();
+        }
+    }
+
+    public static void initConsensus(int nodeId) throws Exception {
+        System.out.println("Iniciado nodo bizantino "+nodeId);
+        JGroupsNode jGroupsNode = new JGroupsNode();
+        ConsensusNode consensusNode = new ConsensusNode(nodeId, jGroupsNode);
+        consensusNode.setNodeView(20000);
+        jGroupsNode.start();
+    }
+}
